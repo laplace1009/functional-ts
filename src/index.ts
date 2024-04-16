0a6064d86bd3f0ea8bab3e3f1020df101605a031
laplace1009@gmail.com <laplace1009@gmail.com>
2024-04-17 03:05:55 +0900

    Functor, Applicative, Monad interface 구현

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
-export type MaybeType<T> = Nothing | Just<T>
+type IsNothingType<T> = T extends undefined ? undefined : T
 
-export class Nothing {
+export interface Functor<T> {
+    map<U>(f:(value: T) => U): Functor<U>
+}
+
+export interface Applicative<T> extends Functor<T>{
+    pure: (value: T) => Applicative<T>
+    applicative<U>(f: (value: T) => U): Applicative<U>
+}
+
+export interface Monad<T> extends Applicative<T>{
+    lift: (value: T) => Monad<T>;
+    bind<U>(f: (value: T) => Monad<U>): Monad<U>
+}
+
+export class Nothing<T> {
     private value: unknown
 
     constructor() {
         this.value = undefined
+    }
+
+    IsNothing(): this is Nothing<T> {
+        return true
+    }
+
+    IsJust(): this is Just<T> {
+        return false
     }
 
     Get() {
@@ -19,13 +41,21 @@ export class Just<T> {
         this.value = value
     }
 
+    IsNothing(): this is Nothing<T> {
+        return false
+    }
+
+    IsJust(): this is Just<T> {
+        return true;
+    }
+
     Get() {
         return this.value
     }
 }
 
-export class Maybe<T> {
-    private value: MaybeType<T>
+export class Maybe<T> implements Monad<T> {
+    private value: Nothing<T> | Just<T>
 
     constructor(value: undefined | T) {
         if (value === undefined) {
@@ -37,5 +67,13 @@ export class Maybe<T> {
 
     Get() {
         return this.value
+    }
+
+    Map<U>(f: (value: T) => U): Maybe<U> {
+        if (this.value.IsNothing()) {
+            return new Maybe<U>(undefined)
+        }
+
+        return new Maybe<U>(f(this.value.Get()))
     }
 }
