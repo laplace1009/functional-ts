941fa3751ff7c724114d74207b268425750fba19
laplace1009 <laplace1009@gmail.com>
2024-05-08 00:29:21 +0900

    약간의 수정

--- src/interfaces/applicative.ts
+++ src/interfaces/applicative.ts
@@ -1,9 +1,6 @@
 import {Functor} from "./functor";
 import {TypeOrReturnType} from "../types/types";
 
-/**
- * pure -> static 으로 구현
- */
 export interface Applicative<T> extends Functor<T> {
     pure<A>(a: A): Functor<A>
     ap<A>(a: A): Functor<TypeOrReturnType<T>>
--- src/interfaces/monad.ts
+++ src/interfaces/monad.ts
@@ -1,11 +1,7 @@
 import {Applicative} from "./applicative";
 import {CallableAtoB} from "../types/types";
 
-/**
- * lift -> static 으로 구현
- */
 export interface Monad<T> extends Applicative<T> {
-    // lift<U>(value: U): Monad<U>
     wrap<A>(a: A): Monad<A>
-    bind<A>(f: CallableAtoB<T, Monad<A>>): Monad<A>
+    bind<A>(f: CallableAtoB<T, Monad<A>>): Monad<T> | Monad<A>
 }
